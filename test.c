4d12f7db3b00a3826e239098e26d327e8fca2b1c
Flip Dreijerink <fdreijer@f1r2s9.codam.nl>
2025-06-16 14:07:55 +0200

    miniminishell

---
+++ test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv, char **envp)
+{
+	char *line;
+	int pid = fork();
+	if (pid == 0)
+		execve("/bin/sh", (char *[]){"sh", "-c", "clear", NULL}, envp);
+	else
+		waitpid(pid, NULL, 0);
+	while (1)
+	{
+		line = readline("[minishell] > ");
+		int pid = fork();
+		if (pid == 0)
+			execve("/bin/sh", (char *[]){"sh", "-c", line, NULL}, envp);
+		else
+			waitpid(pid, NULL, 0);
+	}
+}
