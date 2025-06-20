5ab7a5e65e6a64cb12b695d3bbd5372da0337c5f
Flip Dreijerink <fdreijer@f1r2s8.codam.nl>
2025-06-20 12:15:01 +0200

    strjoin updated

--- libft_mini/ft_strjoin.c
+++ libft_mini/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: fdreijer <fdreijer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 11:02:44 by fdreijer          #+#    #+#             */
-/*   Updated: 2025/06/19 16:14:50 by fdreijer         ###   ########.fr       */
+/*   Updated: 2025/06/20 12:14:45 by fdreijer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,16 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*joined;
 	size_t	i;
 
-	if (!s1 || !s2)
-		return (NULL);
 	joined = ft_calloc(sizeof(char), (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!joined)
 		return (NULL);
 	i = 0;
-	while (s1[i])
+	while (s1 && s1[i])
 	{
 		joined[i] = s1[i];
 		i++;
 	}
-	while (s2[i - ft_strlen(s1)])
+	while (s2 && s2[i - ft_strlen(s1)])
 	{
 		joined[i] = s2[i - ft_strlen(s1)];
 		i++;
