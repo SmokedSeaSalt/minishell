2e956e260b34d57a693e05c7d885026b11152b43
Mathijs Van Rijn <mvan-rij@f1r2s10.codam.nl>
2025-06-20 15:23:39 +0200

    added strjoin_space.c

---
+++ libft_mini/strjoin_space.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       ,--------------      */
+/*                                                      /                     */
+/*                                                     /                      */
+/*                                                    /   ,--,/      ,--      */
+/*                                                    |  /  ,'    ,·´         */
+/*                                                    |  ;·'   ,·´            */
+/*   strjoin_space.c                                  |  ;  ,·´               */
+/*                                                    |  :,'                  */
+/*   By: mvan-rij <mvan-rij@student.42.fr>             `´.                    */
+/*                                                      .¨.                   */
+/*   Created: 2025/06/20 15:20:48 by mvan-rij           ¨· .                  */
+/*   Updated: 2025/06/20 15:23:11 by mvan-rij          :. ¨.                  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_mini.h"
+
+char	*strjoin_path(char *s1, char *s2)
+{
+	char	*joined;
+	int		i;
+	int		j;
+
+	joined = ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 2, 1);
+	if (!joined)
+		return (NULL);
+	i = 0;
+	while (s1 && s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	joined[i] = ' ';
+	i++;
+	j = 0;
+	while (s2 && s2[j])
+	{
+		joined[i + j] = s2[j];
+		j++;
+	}
+	return (joined);
+}
