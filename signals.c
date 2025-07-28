7d98ef92de0aba3e50f1ab9d25f6f4156cf78c3a
Mathijs Van Rijn <mvan-rij@f1r2s10.codam.nl>
2025-07-28 13:10:16 +0200

    added signal init and handler

---
+++ signals.c
@@ -0,0 +1,22 @@
+#include <signal.h> //to use signals
+#include <stddef.h> //to use NULL
+
+volatile int	g_signal_received = 0;
+
+void	signal_handler(int sig)
+{
+	g_signal_received = sig;
+}
+
+int	init_signals(void)
+{
+	struct sigaction	sa;
+	sa.sa_handler = signal_handler;
+	if (sigemptyset(&sa.sa_mask) == -1)
+		return (-1);
+	sa.sa_flags = 0;
+	if (sigaction(SIGINT, &sa, NULL) == -1)
+		return (-1);
+	if (sigaction(SIGQUIT, &sa, NULL) == -1)
+		return (-1);
+}
