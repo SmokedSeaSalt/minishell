369c432a9ecd3ddba90cd7936242239ec493c8e3
SmokedSeaSalt <mathijsrijn@gmail.com>
2025-08-28 11:48:04 +0200

    non numeric and multiple arguments now behave as bash

--- src/built_in/exit_mini.c
+++ src/built_in/exit_mini.c
@@ -6,7 +6,7 @@
 /*   By: mvan-rij <mvan-rij@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 12:26:48 by fdreijer          #+#    #+#             */
-/*   Updated: 2025/08/28 10:50:53 by mvan-rij         ###   ########.fr       */
+/*   Updated: 2025/08/28 11:47:02 by mvan-rij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,11 +103,6 @@ static int	is_numeric(char *str)
 	return (1);
 }
 
-
-
-//TODO different behaviour if exit is piped or not
-//TODO exit a a
-//TODO write exit to 1 so doesnt get piped mayb
 int	exit_mini(t_cmds *cmds)
 {
 	char	exitval;
@@ -116,20 +111,17 @@ int	exit_mini(t_cmds *cmds)
 		write(2, "exit\n", 5);
 	if (n_args(cmds->args) == 0)
 		exit_with_val(ft_atoi(ft_getenv(cmds->info->head, "?")), cmds);
+	if (is_numeric(cmds->args[0]) == 0)
+	{
+		write(2, "minishell: exit: ", 17);
+		write(2, cmds->args[0], ft_strlen(cmds->args[0]));
+		write(2, ": numeric argument required\n", 28);
+		exit_with_val(2, cmds);
+	}
 	if (n_args(cmds->args) == 1)
 	{
-		if (is_numeric(cmds->args[0]) == 1)
-		{
-			exitval = (unsigned char)ft_atoi(cmds->args[0]);
-			exit_with_val(exitval, cmds);
-		}
-		else
-		{
-			write(2, "minishell: exit: ", 17);
-			write(2, cmds->args[0], ft_strlen(cmds->args[0]));
-			write(2, ": numeric argument required\n", 28);
-			exit_with_val(2, cmds);
-		}
+		exitval = (unsigned char)ft_atoi(cmds->args[0]);
+		exit_with_val(exitval, cmds);
 	}
 	if (n_args(cmds->args) > 1)
 		write(2, "minishell: exit: too many arguments\n", 36);
