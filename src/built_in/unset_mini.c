cb051ca7316c26a929836cf57968016b444d1b1f
SmokedSeaSalt <mathijsrijn@gmail.com>
2025-08-29 10:26:07 +0200

    separated into multiple functions

--- src/built_in/unset_mini.c
+++ src/built_in/unset_mini.c
@@ -3,14 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   unset_mini.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fdreijer <fdreijer@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mvan-rij <mvan-rij@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 12:09:01 by fdreijer          #+#    #+#             */
-/*   Updated: 2025/08/26 14:40:30 by fdreijer         ###   ########.fr       */
+/*   Updated: 2025/08/29 10:25:25 by mvan-rij         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "built_in.h"
+
+static void	unlink_node_and_free(t_cmds *cmds, t_env *env, int i)
+{
+	if (!ft_strcmp(cmds->args[i], env->v_name))
+	{
+		if (env->prev)
+			env->prev->next = env->next;
+		else
+			cmds->info->head = env->next;
+		if (env->next)
+			env->next->prev = env->prev;
+		free(env->v_name);
+		env->v_name = NULL;
+		free(env->v_val);
+		env->v_val = NULL;
+		free(env);
+		env = NULL;
+	}
+}
 
 int	unset_mini(t_cmds *cmds)
 {
@@ -23,22 +42,7 @@ int	unset_mini(t_cmds *cmds)
 		env = cmds->info->head;
 		while (env)
 		{
-			if (!ft_strcmp(cmds->args[i], env->v_name))
-			{
-				if (env->prev)
-					env->prev->next = env->next;
-				else
-					cmds->info->head = env->next;
-				if (env->next)
-					env->next->prev = env->prev;
-				free(env->v_name);
-				env->v_name = NULL;
-				free(env->v_val);
-				env->v_val = NULL;
-				free(env);
-				env = NULL;
-				return (0);
-			}
+			unlink_node_and_free(cmds, env, i);
 			env = env->next;
 		}
 		i++;
